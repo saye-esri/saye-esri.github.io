6e23114e9622e3557de5bc85853bcfe0524e094d
saye-esri <saye@esri.ca>
2018-09-17 13:07:03 -0400

    fixed redirect

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	var client_id = "cDEbMgKnRUKN85YW";
-	var redirect_uri =  "saye-esri.github.io/form";
+	var redirect_uri =  "https://saye-esri.github.io/form";
 	window.location.href = `https://www.arcgis.com/sharing/rest/oauth2/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`
 });
