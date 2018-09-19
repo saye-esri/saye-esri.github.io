1ee82dc1b3b35cd6f22b4f0bd07423413941af1a
saye-esri <saye@esri.ca>
2018-09-19 15:23:40 -0400

    changed stuff

--- map/script.js
+++ map/script.js
@@ -1,8 +1,6 @@
 var map = L.map("map").setView([0, 0], 12);
 L.esri.basemapLayer("Topographic").addTo(map);
 
-
-
 var group = L.featureGroup();
 group.addTo(map);
 
@@ -16,6 +14,17 @@ var in_orders_p = $.getJSON(`https://logistics.arcgis.com/arcgis/rest/services/W
 
 var in_depots_p = $.getJSON(`https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem("jobid")}/inputs/depots?f=json&token=${sessionStorage.getItem("token")}`);
 
+function addSpace(string) {
+  return string.replace(/([A-Z])/g, ' $1').trim();
+}
+
+function getColor(d) {
+  return d === 'Orders'  ? "#de2d26" :
+         d === 'Depots'  ? "#377eb8" :
+         d === 'Stops' ? "#4daf4a" :
+         d === 'Roadside Hazards' ? "#984ea3" :
+                      "#ff7f00";
+}
 
 function makeColor() {
 	var color;
@@ -40,22 +49,21 @@ function addGeometry(orders, depots, stops) {
 function addToMap(geoJson, layer, color) {
   L.geoJson(geoJson, {
     pointToLayer: function(feature, latlng) {
-      if (feature.geometry.type == "Point") return L.circleMarker(latlng, {radius: 9});
+      if (feature.geometry.type == "Point") return L.circleMarker(latlng, {radius: 8});
     },
     style: function(feature) {
     	console.log(feature);
     	if (color) {
   		return {stroke: false, fill: true, color: color, fillOpacity: 0.8};
     	} else {
-    	return {color: makeColor()};
+    	return {weight: 4, color: makeColor()};
     	}	
     },
     onEachFeature: function(feature, layer) {
       var popupContent = "<table>";
       for (var p in feature.properties) {
-        (typeof feature.properties[p] === "number") ? 
-          popupContent += "<tr><td>" + p + "</td><td>" + String(feature.properties[p]).substring(0,6) + "</td></tr>" : 
-          popupContent += "<tr><td>" + p + "</td><td>" + feature.properties[p] + "</td></tr>"
+
+          popupContent += "<tr><td>" + addSpace(p) + "</td><td>" + feature.properties[p] + "</td></tr>"
       }
       popupContent += "</table>"
       layer.bindPopup(popupContent);
@@ -70,11 +78,11 @@ out_routes_p.done(function(data) {
 });
 
 in_depots_p.done(function(data) {
-  addToMap(L.esri.Util.arcgisToGeoJSON(data.value), group, "#de2d26");
+  addToMap(L.esri.Util.arcgisToGeoJSON(data.value), group, getColor('Depot'));
 });
 
 in_orders_p.done(function(data) {
-  addToMap(L.esri.Util.arcgisToGeoJSON(data.value), group, "#377eb8");
+  addToMap(L.esri.Util.arcgisToGeoJSON(data.value), group, getColor('Orders'));
 });
 
 Promise.all([in_orders_p, in_depots_p, out_stops_p]).then(function(lst){
@@ -82,8 +90,7 @@ Promise.all([in_orders_p, in_depots_p, out_stops_p]).then(function(lst){
   var in_depots = L.esri.Util.arcgisToGeoJSON(lst[1].value);
   var out_stops = L.esri.Util.arcgisToGeoJSON(lst[2].value);
   addGeometry(in_orders, in_depots, out_stops);
-  addToMap(out_stops, stops, "#4daf4a");
-  
+  addToMap(out_stops, stops, getColor('Stops'));
 });
 
 var overlayStops = {
@@ -91,13 +98,7 @@ var overlayStops = {
 }; 
 L.control.layers(null, overlayStops).addTo(map);
 
-function getColor(d) {
-  return d === 'Orders'  ? "#de2d26" :
-         d === 'Depots'  ? "#377eb8" :
-         d === 'Stops' ? "#4daf4a" :
-         d === 'Roadside Hazards' ? "#984ea3" :
-                      "#ff7f00";
-};
+
 
 
 
