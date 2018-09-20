ca4182320c29020069f8c99342eb9b760a9ec223
saye-esri <saye@esri.ca>
2018-09-20 11:51:00 -0400

    gg

--- map/script.js
+++ map/script.js
@@ -133,6 +133,7 @@ function addToMap(geoJson, layer, color) {
                             <tbody>`;
       for (p in feature.properties) {
         console.log(p);
+        console.log(include.p);
         if (include.p) {
           popupContent += "<tr><td>" + addSpace(String(p)) + "</td><td>" + unit.assign(p, feature.properties[p]) + "</td></tr>"
         }
