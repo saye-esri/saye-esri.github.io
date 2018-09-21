900fa403d003ac4f22ee5f7526c8ee7a2c67b25d
saye-esri <saye@esri.ca>
2018-09-21 11:08:14 -0400

    changed jquery

--- form/buttons.js
+++ form/buttons.js
@@ -36,7 +36,7 @@ $(document).ready(function(){
             console.log(key);
             var utc = Date.parse(key);
             var timestamp = new Date(utc);
-            $('#joblist').after(`<a href="/processing" class="list-group-item list-group-item-action historyButton" id="${history[key]}>Job on ${timestamp.toDateString()} at ${timestamp.toTimeString()}</a>`)
+            $('#joblist').append(`<a href="/processing" class="list-group-item list-group-item-action historyButton" id="${history[key]}>Job on ${timestamp.toDateString()} at ${timestamp.toTimeString()}</a>`);
         }
 
 
