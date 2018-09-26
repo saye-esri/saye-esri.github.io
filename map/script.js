68a80306c31f311d5bb8a2792e15999b38118d2c
saye-esri <saye@esri.ca>
2018-09-26 11:50:50 -0400

    y

--- map/script.js
+++ map/script.js
@@ -6,12 +6,13 @@ require([
 ], function(
   Map,
   MapView,
+  domReady,
   FeatureLayer
 ) {
 
   const layer = new FeatureLayer({
   // URL to the service
-  url: "https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/ja6f1d378ba31413281c3549d86750e40/results/out_routes?f=json&token=qyKtAAGxxI_dhtgs7fWBJmCOchiSFWB9LG4gg41UIYQPe8LOXyBsx0nrtoBnZDOsx8hwS1C5319legeQKAauR70BhAQvYsaVsCm43-4Z-0pqhcUmG2suOPQjnnQ-rPS13hKtEIn7hUwN0sfmvs0rmUB6n1MwU2vJjjQCTCiDPE1d2Nc-ezDKe0qzNE4BhKb6Qi0bVTbv5BT4Jfyk9nAnjQ.."
+  url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/0"
   });
 
   // Create the Map
