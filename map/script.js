0d823aa4aaff4f79e7014c10d31226978eeed2a2
saye-esri <saye@esri.ca>
2018-09-26 11:54:24 -0400

    changed layer

--- map/script.js
+++ map/script.js
@@ -12,7 +12,7 @@ require([
 
   const layer = new FeatureLayer({
   // URL to the service
-  url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/0"
+  url: "https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/ja6f1d378ba31413281c3549d86750e40/results/out_routes?f=json&token=GABxl1Pw59gjtdkBb5a9Ly9_XSuCwWlpNLEVB2UGSHIlQIlSMjItSBhXbtPcVbwf-D6saOdNjOh7e9hawx3hlSVbVgr_t3O0iNgcTtyNME310xM9xBMNDsKWKNzjy-uUcOMPpqdQKD4doAeUUOsODijfX2CQmnxSR7rDGOotjZ-BfJvIL3bEkAV8hMSEtDM_issKFsXEJTGXTUHHZ10pmw.."
   });
 
   // Create the Map
