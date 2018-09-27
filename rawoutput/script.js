97cee22805c38af80cec2d18561c93432662aeea
saye-esri <saye@esri.ca>
2018-09-27 13:17:46 -0400

    ?????

--- rawoutput/script.js
+++ rawoutput/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var url = `https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem('jobid')}/${sessionStorage.getItem('json')}/?f=json&token=${sessionStorage.getItem('token')}`
 	$.getJSON(url, function(data) {
-		$('.container').html(JSON.stringify(data));
+		$('#container').html(JSON.stringify(data, null, 4));
 	});
 });
