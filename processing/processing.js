8bb17c8248f5cb8f8aa327723de45c8cdf4d9921
saye-esri <saye@esri.ca>
2018-09-27 12:49:06 -0400

    maybe

--- processing/processing.js
+++ processing/processing.js
@@ -20,8 +20,8 @@ $(document).ready(function() {
         }
         console.log(outLst);
         for (key in outLst){
-            var ddURL = `https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem('jobid')}/${outLst[key]["paramUrl"]}?f=json&token=${sessionStorage.getItem('token')}`
-            var ddItem = `<a class="dropdown-item" href="${ddURL}">${key}</a>`
+            //var ddURL = `https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem('jobid')}/${outLst[key]["paramUrl"]}?f=json&token=${sessionStorage.getItem('token')}`
+            var ddItem = `<a class="dropdown-item" href="/rawoutput/?type=${outLst[key]['paramUrl']}">${key}</a>`
             $('#ddItem').append(ddItem);
         }
         $('#rawJSON').prop('disabled', false);
---
+++ rawoutput/script.js
@@ -0,0 +1,34 @@
+function parseURLParams(url) {
+    var queryStart = url.indexOf("?") + 1,
+        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+        query = url.slice(queryStart, queryEnd - 1),
+        pairs = query.replace(/\+/g, " ").split("&"),
+        parms = {}, i, n, v, nv;
+
+    if (query === url || query === "") {
+        return;
+    }
+
+    for (i = 0; i < pairs.length; i++) {
+        nv = pairs[i].split("=");
+        n = decodeURIComponent(nv[0]);
+        v = decodeURIComponent(nv[1]);
+
+        if (!parms.hasOwnProperty(n)) {
+            parms[n] = [];
+        }
+
+        parms[n].push(nv.length === 2 ? v : null);
+    }
+    return parms;
+}
+
+var params = parseURLParams(window.location.href);
+console.log(params);
+
+$(document).ready(function() {
+	$.getJSON(`https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem('jobid')}/${params.type}?f=json&token=${sessionStorage.getItem('token')}`,
+		function(data) {
+			$('.container').html(JSON.stringify(data));
+		})
+});
