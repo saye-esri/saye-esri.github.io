e649efdcf9f708e30f693e49d1517cd05b83c0e7
saye-esri <saye@esri.ca>
2018-09-28 13:29:08 -0400

    dbdfb

---
+++ map/script2.js
@@ -0,0 +1,40 @@
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "dojo/domReady!",
+  "esri/layers/FeatureLayer",
+  "esri/tasks/Geoprocessor"
+], function(
+  Map,
+  MapView,
+  domReady,
+  FeatureLayer,
+  Geoprocessor
+) {
+
+  var url = `https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem("jobid")}/results/out_routes/`
+  var mytoken = sessionStorage.getItem('token')
+
+
+  var layer = new FeatureLayer(url,{
+    id: 'out_routes',
+    mode: FeatureLayer.MODE_ONDEMAND,
+    outFields: ["*"],
+    token: mytoken
+  });
+
+  // Create the Map
+  var map = new Map({
+    basemap: "streets-navigation-vector",
+    layers: [layer]
+  });
+
+  // Create the MapView
+  var view = new MapView({
+    container: "map",
+    map: map,
+    center: [-80, 35],
+    zoom: 3
+  });
+  
+});
