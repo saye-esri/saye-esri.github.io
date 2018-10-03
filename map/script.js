cf98ad988e3a6b21048eb32d59ba19f882b0632a
saye-esri <saye@esri.ca>
2018-10-03 13:47:48 -0400

    new templates

--- map/script.js
+++ map/script.js
@@ -1,7 +1,26 @@
 var out_routes_p = $.getJSON(`https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem("jobid")}/results/out_routes?f=json&token=${sessionStorage.getItem("token")}`);
+var in_orders_p = $.getJSON(`https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem("jobid")}/inputs/orders?f=json&token=${sessionStorage.getItem("token")}`);
+var in_depots_p = $.getJSON(`https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem("jobid")}/inputs/depots?f=json&token=${sessionStorage.getItem("token")}`);
+
 
 function getRand() {
   return Math.floor((Math.random() * 256));
+}
+
+function makeTemplate(feature) {
+  var template = {
+    title: "{Name}",
+    content: [{
+      type: "fields",
+      fieldInfos: []
+    }]
+  }
+
+  for (key in feature.attributes) {
+    template.content[0].fieldInfos.push({fieldName: key})
+  }
+
+  return template
 }
 
 require([
@@ -10,7 +29,8 @@ require([
   "dojo/domReady!",
   "dojo/_base/array",
   "esri/Graphic",
-  "esri/symbols/SimpleLineSymbol"
+  "esri/symbols/SimpleLineSymbol",
+  "esri/symbols/SimpleMarkerSymbol"
 ], function(
   Map,
   MapView,
@@ -33,34 +53,33 @@ require([
     zoom: 3
   });
 
-  var template = {
-    title: "{Name}",
-    content: [{
-      type: "fields",
-      fieldInfos: [{
-        fieldName: "Name"
-      }, {
-        fieldName: "StartTime"
-      }, {
-        fieldName: "EndTime"
-      }, {
-        fieldName: "TotalCost"
-      }]
-    }]
-  }
+  
 
   out_routes_p.done(function(data) {
     array.forEach(data.value.features, function(feature) {
-      var lineSymbol = new SimpleLineSymbol({
+      var symbol = new SimpleLineSymbol({
         color: [getRand(), getRand(), getRand()],
         width: 4
       });
       var graphic = Graphic.fromJSON(feature);
-      graphic.popupTemplate = template;
-      graphic.symbol = lineSymbol;
+      graphic.popupTemplate = makeTemplate(feature);
+      graphic.symbol = symbol;
       view.graphics.add(graphic);
     }, this);
   });
 
-   
+  in_orders_p.done(function(data) {
+    array.forEach(data.value.features, function(feature) {
+      var symbol = new SimpleMarkerSymbol({
+        color: [20, 255, 20],
+        size: '8px'
+      });
+      var graphic = Graphic.fromJSON(feature);
+      graphic.popupTemplate = makeTemplate(feature);
+      graphic.symbol = symbol;
+      view.graphics.add(graphic);
+    }, this);
+  });
+
+
 });
