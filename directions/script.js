03dd92d878b008a4d31b2bb56c985b628f6284d8
saye-esri <saye@esri.ca>
2018-10-17 10:38:20 -0400

    erg

--- directions/script.js
+++ directions/script.js
@@ -130,7 +130,6 @@ $(document).ready(function() {
 			for (key in thisObject) {
 				href += `stop=${thisObject[key].lat},${thisObject[key].long}&stopname=${thisObject[key].stopName}&`
 			}
-			con
 			$(this).prop('href', href.slice(0, -1));
 			$(this).prop('disabled', false);
 		});
