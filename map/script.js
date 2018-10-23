ac32a6fe72f02bcc6e6e26eeae0749d8f1a3224e
saye-esri <saye@esri.ca>
2018-10-23 09:10:19 -0400

    sdfsdf

--- map/script.js
+++ map/script.js
@@ -121,7 +121,7 @@ require([
     },
     labelPlacement: 'center-right',
     labelExpressionInfo: {
-      expression: `IIF($feature.StopType == '0', $feature.Sequence + ', ' + $feature.ArriveTime, 'hello')`
+      expression: `IIF($feature.StopType == 0, $feature.Sequence + ', ' + $feature.ArriveTime, '')`
     }
   };
 
