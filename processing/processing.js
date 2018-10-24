d8edbf0bc8ec19867647dc6ec5c33001b891496e
saye-esri <saye@esri.ca>
2018-10-24 15:12:09 -0400

    made it lit

--- processing/processing.js
+++ processing/processing.js
@@ -1,3 +1,5 @@
+var processTimer, uploadTimer;
+
 function sendToAGOL(name, geodatabase) {
     $.ajax({ //Create item in portal
         url: `https://www.arcgis.com/sharing/rest/content/users/${sessionStorage.getItem('user')}/addItem`,
@@ -13,124 +15,137 @@ function sendToAGOL(name, geodatabase) {
         },
         success: function(result) {
             console.log(result);
-            var uploadTimer;
-            function publish(name, itemID) {
-                $.ajax({
-                    url: `https://www.arcgis.com/sharing/rest/content/users/${sessionStorage.getItem('user')}/publish`,
-                    type: "post",
-                    dataType: "json",
-                    data: {
-                        itemID: itemID,
-                        overwrite: true,
-                        fileType: 'fileGeodatabase',
-                        publishParameters: `{"name":"${name}"}`,
-                        token: sessionStorage.getItem('token'),
-                        f: "pjson"
-                    },
-                    success: function(result2) {
-                        console.log(result2);
-                    }
-                });
-            }
-            function checkUpload(itemID) {
-                $.ajax({
-                    url: `https://www.arcgis.com/sharing/rest/content/users/${sessionStorage.getItem('user')}/items/${itemID}/status`,
-                    type: "post",
-                    dataType: 'json',
-                    data: {
-                        token: sessionStorage.getItem('token'),
-                        f: 'pjson'
-                    },
-                    success: function(statusResult) {
-                        if (statusResult.status === "completed") {
-                            if (uploadTimer) clearInterval(uploadTimer);
-                            publish(name, statusResult.itemId);
-                        } else if (statusResult.status === "processing") {
-                            return false;
-                        }
-                    }
-                })
-            }
             if (!checkUpload(result.id)) {
                 uploadTimer = setInterval(function() {
                     checkUpload(result.id)
                 }, 1000);
-            };
-        }
-    });
-}
-
-$(document).ready(function() {
-
-    $('#tooltip').tooltip('disable');
-
-    var checkURL = `https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem('jobid')}?returnMessages=true&f=pjson&token=${sessionStorage.getItem('token')}`
-
-    var myP = `Your job ID is: ${sessionStorage.getItem('jobid')} <a id="copy" class="text-primary">copy to clipboard</a><br>Job JSON can be found <a href="${checkURL}">here</a><br>`;
-
-    $('#replace').html(myP);
-
-    function rawJSON(data) {
-        var outLst = {};
-        console.log(data);
-        for (key in data) {
-            if (typeof(data[key]) != 'object') continue;
-            for (key2 in data[key]) {
-                if (!("paramUrl" in data[key][key2])) continue;
-                outLst[key2] = data[key][key2];
             }
         }
-        console.log(outLst);
-        for (key in outLst){
-            var ddURL = `https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem('jobid')}/${outLst[key]["paramUrl"]}?f=pjson&token=${sessionStorage.getItem('token')}`
-            var ddItem = `<a class="dropdown-item" href="${ddURL}">${key}</a>`
-            $('#ddItem').append(ddItem);
-        }
-        $('#rawJSON').prop('disabled', false);
-    }
+    });
+};
 
-    var timer;
-    function checkData() {
-        $.getJSON(checkURL, function(data) {
-            var realError = JSON.stringify(data).includes('WARNING 030088');
-            var n = sessionStorage.getItem('AGOLName')
-            if (data.jobStatus == "esriJobSucceeded" && !(realError)) {
-                if (timer) clearInterval(timer);
-                if (n) {
-                    $.getJSON(`https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem('jobid')}/results/out_route_data?f=pjson&token=${sessionStorage.getItem('token')}`, function(data) {
-                        sendToAGOL(n, data.value.url);
-                    });
-                }
-                $('#viewMap').prop('disabled', false);
-                $('#message').prop('class', 'text-success').html('Job completed successfully!');
-                $('#canDelete').html('See job status below');
-                $('h1').html('Processing Complete');
-                rawJSON(data);
-                $.getJSON(`https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem('jobid')}/results/out_directions?f=pjson&token=${sessionStorage.getItem('token')}`, function(data) {
-                    if (data.value.features.length > 0) {
-                        $('#viewDir').prop('disabled', false);
-                    } else {
-                    $('#viewDir').css('pointer-events', 'none');
-                    $('#tooltip').tooltip('enable');
-                    }
-                });
-            } else if (data.jobStatus == "esriJobFailed" || data.jobStatus == "esriJobTimedOut" || realError) {
-                $('h1').html('Processing Complete');
-                $('#canDelete').html('See job status below');
-                $('#message').prop('class', 'text-danger').html('Job failed, view JSON for more details.');
-                if (timer) clearInterval(timer);
-                rawJSON(data);
-            } else if (data.error.message == "Invalid Token") {
-                alert('Invalid Token');
-                window.location.href = "/";
-            }else return false;
-        });
+function publish(name, itemID) {
+    $.ajax({
+        url: `https://www.arcgis.com/sharing/rest/content/users/${sessionStorage.getItem('user')}/publish`,
+        type: "post",
+        dataType: "json",
+        data: {
+            itemID: itemID,
+            overwrite: true,
+            fileType: 'fileGeodatabase',
+            publishParameters: `{"name":"${name}"}`,
+            token: sessionStorage.getItem('token'),
+            f: "pjson"
+        },
+        success: function(result2) {
+            $('#progressbar').css('width', '100%').removeClass('progress-bar-animated');
+            $('#progresslabel').html('Solution found and data published to ArcGIS Online');
+            console.log(result2);
+        }
+    });
+};
+
+function checkUpload(itemID) {
+    $.ajax({
+        url: `https://www.arcgis.com/sharing/rest/content/users/${sessionStorage.getItem('user')}/items/${itemID}/status`,
+        type: "post",
+        dataType: 'json',
+        data: {
+            token: sessionStorage.getItem('token'),
+            f: 'pjson'
+        },
+        success: function(statusResult) {
+            if (statusResult.status === "completed") {
+                if (uploadTimer) clearInterval(uploadTimer);
+                $('#progressbar').css('width', '75%');
+                $('progresslabel').html('Publishing Features');
+                publish(name, statusResult.itemId);
+            } else if (statusResult.status === "processing") {
+                return false;
+            }
+        }
+    });
+};
+
+function rawJSON(data) {
+    var outLst = {};
+    console.log(data);
+    for (key in data) {
+        if (typeof(data[key]) != 'object') continue;
+        for (key2 in data[key]) {
+            if (!("paramUrl" in data[key][key2])) continue;
+            outLst[key2] = data[key][key2];
+        }
     }
-    if (!(checkData())) {
-        var timer = setInterval(function() {
+    console.log(outLst);
+    for (key in outLst){
+        var ddURL = `https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem('jobid')}/${outLst[key]["paramUrl"]}?f=pjson&token=${sessionStorage.getItem('token')}`
+        var ddItem = `<a class="dropdown-item" href="${ddURL}">${key}</a>`
+        $('#ddItem').append(ddItem);
+    }
+    $('#rawJSON').prop('disabled', false);
+};
+
+function complete() {
+    $('#viewMap').prop('disabled', false);
+    $('#message').prop('class', 'text-success').html('Job completed successfully!');
+    $('#canDelete').html('See job status below');
+    $('h1').html('Processing Complete');
+    rawJSON(data);
+    $.getJSON(`https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem('jobid')}/results/out_directions?f=pjson&token=${sessionStorage.getItem('token')}`, function(data) {
+        if (data.value.features.length > 0) {
+            $('#viewDir').prop('disabled', false);
+        } else {
+        $('#viewDir').css('pointer-events', 'none');
+        $('#tooltip').tooltip('enable');
+        }
+    });
+};
+
+function checkData(checkURL) {
+    $.getJSON(checkURL, function(data) {
+        var realError = JSON.stringify(data).includes('WARNING 030088');
+        var n = sessionStorage.getItem('AGOLName')
+        if (data.jobStatus == "esriJobSucceeded" && !(realError)) {
+            if (processTimer) clearInterval(processTimer);
+            if (n) {
+                $.getJSON(`https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem('jobid')}/results/out_route_data?f=pjson&token=${sessionStorage.getItem('token')}`, function(data) {
+                    sendToAGOL(n, data.value.url);
+                });
+            } else {
+                complete();
+            }
+            
+        } else if (data.jobStatus == "esriJobFailed" || data.jobStatus == "esriJobTimedOut" || realError) {
+            $('h1').html('Processing Complete');
+            $('#canDelete').html('See job status below');
+            $('#message').prop('class', 'text-danger').html('Job failed, view JSON for more details.');
+            if (processTimer) clearInterval(processTimer);
+            rawJSON(data);
+        } else if (data.error.message == "Invalid Token") {
+            alert('Invalid Token');
+            window.location.href = "/";
+        } else return false;
+    });
+};
+
+$(document).ready(function() {
+    $('#tooltip').tooltip('disable');
+    var checkURL = `https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblem/GPServer/SolveVehicleRoutingProblem/jobs/${sessionStorage.getItem('jobid')}?returnMessages=true&f=pjson&token=${sessionStorage.getItem('token')}`
+    var myP = `Job JSON can be found <a href="${checkURL}">here</a><br>`;
+    $('#replace').html(myP);
+
+    if(sessionStorage.getItem('AGOLName')) {
+        $('#progressbar').css('width', '25%');
+    } else {
+        $('#progressbar').css('width', '50%');
+    }
+    
+    if (!(checkData(checkURL))) {
+        processTimer = setInterval(function() {
             checkData();
-        }, 3000);
-    };
+        }, 1000);
+    }
     
     $('#viewMap').click(function(){
         window.location.href = '/map';
@@ -138,26 +153,5 @@ $(document).ready(function() {
 
     $('#viewDir').click(function() {
         window.location.href = '/directions';
-    });
-
-    $('#copy').click(function() {
-        const el = document.createElement('textarea');  // Create a <textarea> element
-        el.value = sessionStorage.getItem('jobid');                                 // Set its value to the string that you want copied
-        el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
-        el.style.position = 'absolute';                 
-        el.style.left = '-9999px';                      // Move outside the screen to make it invisible
-        document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
-        const selected =            
-            document.getSelection().rangeCount > 0        // Check if there is any content selected previously
-                ? document.getSelection().getRangeAt(0)     // Store selection if found
-                : false;                                    // Mark as false to know no selection existed before
-        el.select();                                    // Select the <textarea> content
-        document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
-        document.body.removeChild(el);                  // Remove the <textarea> element
-        if (selected) {                                 // If a selection existed before copying
-            document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
-            document.getSelection().addRange(selected);   // Restore the original selection
-        }
-        $(this).html('copied!')
     });
 });
