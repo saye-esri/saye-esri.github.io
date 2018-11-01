0b01093e3a69671589e40bc98e429b9798353d49
saye-esri <saye@esri.ca>
2018-11-01 09:55:56 -0400

    njn

--- form/myscript.js
+++ form/myscript.js
@@ -593,7 +593,7 @@ $(document).ready(function(){
         var utc = Date.parse(key);
         var timestamp = new Date(utc);
         if (now.getTime() > timestamp.getTime() + oneDay) continue;
-        var newhtml = `<a href="/processing" class="dropdown-item historyButton" id="${history[key][id]}" value="${history[key][params]}">Job on ${timestamp.toDateString()} at ${timestamp.toTimeString().slice(0,8)}</a>`;
+        var newhtml = `<a href="/processing" class="dropdown-item historyButton" id="${history[key]['id']}" value="${history[key]['params']}">Job on ${timestamp.toDateString()} at ${timestamp.toTimeString().slice(0,8)}</a>`;
         $('#joblist').append(newhtml);
         newJobHistory[timestamp] = history[key];
     }
