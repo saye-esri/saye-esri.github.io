e1b7c9c100b86a091846964d21ded958b0c2e908
Sean Aye <saye@esri.ca>
2018-12-10 14:39:00 -0500

    Changed to Vue

--- redirect.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	var client_id = "cDEbMgKnRUKN85YW";
-	var redirect_uri =  "https://saye-esri.github.io/form/?";
-	window.location.href = `https://www.arcgis.com/sharing/rest/oauth2/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`
-});
